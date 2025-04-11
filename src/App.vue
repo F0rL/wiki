7edf2dbcabdc279db923f81177824e63251ae8e6
F0rL <kumaloveslife@163.com>
2025-04-11 16:28:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
